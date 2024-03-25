ec34565e3240281282f135f2bd0e8c513b0cdb42
danMihel <90636062+danMihel@users.noreply.github.com>
2024-03-25 16:18:29 +0300

    Update index.vue

--- components/common/image/index.vue
+++ components/common/image/index.vue
@@ -1,24 +1,25 @@
 <template>
   <picture>
-    <source :srcset="data?.smallImg" media="(max-width: 750px)">
-    <img :src="data?.bigImg">
+    <source :srcset="data.smallImg" media="(max-width: 750px)">
+    <img :src="data.bigImg">
   </picture>
 </template>
-<script>
 
+<script>
 export default {
   name: 'image-component',
   props: {
     data: {
       type: Object,
-      default: () => {}
+      required: true
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  img {
-    object-fit: cover;
-  }
+@import "@/styles/your-styles";
+img {
+  object-fit: cover;
+}
 </style>
